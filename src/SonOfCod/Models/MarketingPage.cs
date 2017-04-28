32030f248e82a66716d587d25284482756505356
Elise St Hilaire <eliseylenore@gmail.com>
2017-04-28 13:07:31 -0700

    Create product and marketing page models

---
+++ src/SonOfCod/Models/MarketingPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SonOfCod.Models
+{
+    [Table("MarketingPages")]
+    public class MarketingPage
+    {
+        public string BannerImageUrl { get; set; }
+        public string BannerTitle { get; set; }
+        public string SummaryTitle { get; set; }
+        public string SummaryText { get; set; }
+        public string SummaryImageUrl { get; set; }
+        public string ProductsTitle { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+        public string NewsTitle { get; set; }
+        public string NewsSummary { get; set; }
+        public string NewsImageUrl { get; set; }
+    }
+}
---
+++ src/SonOfCod/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SonOfCod.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
